38624fc98d7eb0fd1a0fb92704779dfb76022801
Ayberk Karaköse <132479583+akarakosee@users.noreply.github.com>
2024-12-25 21:35:37 +0300

    Add files via upload

---
+++ Areas/Admin/Controllers/SalonsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KuaforApp.Areas.Admin.Controllers
+{
+    public class SalonsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
